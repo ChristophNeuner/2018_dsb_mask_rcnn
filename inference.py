8179a83d808ab54936a0aa7c622d6c56666a2c60
ChristophNeuner <christoph_neuner@web.de>
2018-04-28 15:07:11 +0000

    add methods to inference.py

--- inference.py
+++ inference.py
@@ -9,10 +9,10 @@ import numpy as np
 from tqdm import tqdm
 from inference_config import inference_config
 from bowl_dataset import BowlDataset
-from mrcnn.utils import rle_encode, rle_decode, rle_to_string
 import functions as f
 ROOT_DIR = os.getcwd()
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
 
 # Recreate the model in inference mode
 model = modellib.MaskRCNN(mode="inference", 
@@ -51,4 +51,40 @@ for image_id in tqdm(sample_submission.ImageId):
 
 
 
-f.write2csv('submission_v2_final_9_50_correct.csv', ImageId, EncodedPixels)
+f.write2csv('submission_v2_final_test123.csv', ImageId, EncodedPixels)
+
+
+
+def rle_encode(x):
+    '''
+    x: numpy array of shape (height, width), 1 - mask, 0 - background
+    Returns run length as list
+    '''
+    dots = np.where(x.T.flatten()==1)[0] # .T sets Fortran order down-then-right
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+
+
+def rle_to_string(runs):
+    return ' '.join(str(x) for x in runs)
+
+
+def rle_decode(mask_rle, shape):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[1]*shape[0], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape((shape[1], shape[0])).T
