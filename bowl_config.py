fd8f588d6b9455660a7d75732fca412fcc71cff4
ChristophNeuner <christoph_neuner@web.de>
2018-04-28 14:48:36 +0000

    initial

---
+++ bowl_config.py
@@ -0,0 +1,209 @@
+from mrcnn.config import Config
+from bowl_dataset import BowlDataset
+import numpy as np
+
+
+
+# Training dataset
+dataset_train = BowlDataset()
+dataset_train.load_bowl('stage1_train_fixed')
+#dataset_train.load_bowl('stage1_train')
+dataset_train.prepare()
+print("Images: {} nClasses: {}".format(len(dataset_train.image_ids), dataset_train.class_names))
+# Validation dataset
+dataset_val= BowlDataset()
+dataset_val.load_bowl('extra_data')
+dataset_val.prepare()
+print("Images: {} nClasses: {}".format(len(dataset_val.image_ids), dataset_val.class_names))
+
+
+
+    
+
+#10th try, with jakob's code
+###config
+class BowlConfig(Config):    
+    # Give the configuration a recognizable name
+    NAME = "bowl"
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1 # background + nuclei
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    # Image mean (RGB)
+    MEAN_PIXEL = np.array([123.7, 116.8, 103.9])    
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+    TRAIN_ROIS_PER_IMAGE = 600   
+    STEPS_PER_EPOCH = len(dataset_train.image_ids)/(GPU_COUNT*IMAGES_PER_GPU)
+    VALIDATION_STEPS = len(dataset_val.image_ids)/(GPU_COUNT*IMAGES_PER_GPU)
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
+    MAX_GT_INSTANCES = 256
+    DETECTION_MAX_INSTANCES = 512    
+    RESNET_ARCHITECTURE = "resnet50"    
+    LEARNING_RATE = 0.0001
+    LEARNING_MOMENTUM = 0.9
+    # Decrease the LR if it has not decreased in a given number of epochs
+    # See keras.callbacks.ReduceLROnPlateau
+    LR_PLATEAU = {
+        "monitor": 'val_loss',
+        "factor": 0.5,
+        "patience": 5,
+        "verbose": 1,
+        "mode": 'auto',
+        "epsilon": 0.0001,
+        "cooldown": 5,
+        "min_lr": 1e-7
+    }
+
+""" 
+#9th try, with jakob's code
+###config
+class BowlConfig(Config):    
+    # Give the configuration a recognizable name
+    NAME = "bowl"
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1 # background + nuclei
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    # Image mean (RGB)
+    MEAN_PIXEL = np.array([123.7, 116.8, 103.9])    
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+    TRAIN_ROIS_PER_IMAGE = 600   
+    STEPS_PER_EPOCH = len(dataset_train.image_ids)/(GPU_COUNT*IMAGES_PER_GPU)
+    VALIDATION_STEPS = len(dataset_val.image_ids)/(GPU_COUNT*IMAGES_PER_GPU)
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
+    MAX_GT_INSTANCES = 256
+    DETECTION_MAX_INSTANCES = 512    
+    RESNET_ARCHITECTURE = "resnet101"    
+    LEARNING_RATE = 0.0001
+    LEARNING_MOMENTUM = 0.9
+    # Decrease the LR if it has not decreased in a given number of epochs
+    # See keras.callbacks.ReduceLROnPlateau
+    LR_PLATEAU = {
+        "monitor": 'val_loss',
+        "factor": 0.5,
+        "patience": 5,
+        "verbose": 1,
+        "mode": 'auto',
+        "epsilon": 0.0001,
+        "cooldown": 5,
+        "min_lr": 1e-7
+    }
+
+
+#8th try: first step: heads only, 25 epochs, second step: all layers, 50 epochs, 1/10*learning_rate, coco, score:
+#7th try: first step: heads only, 50 epochs, second step: all layers, 100 epochs, 1/10*learning_rate, coco, score: 0.377
+#6th try: all layers, 100 epochs, coco, score: 0.392
+
+class BowlConfig(Config): 
+    NAME = "bowl" 
+    GPU_COUNT = 1 
+    IMAGES_PER_GPU = 2 
+    NUM_CLASSES = 1 + 1 
+    IMAGE_MIN_DIM = 256 
+    IMAGE_MAX_DIM = 512 
+    RPN_ANCHOR_SCALES = (4, 8, 16, 32, 64)
+    TRAIN_ROIS_PER_IMAGE = 500 
+    STEPS_PER_EPOCH = 600 // (IMAGES_PER_GPU * GPU_COUNT) 
+    VALIDATION_STEPS = 70 // (IMAGES_PER_GPU * GPU_COUNT) 
+    MEAN_PIXEL = [0, 0, 0] 
+    LEARNING_RATE = 0.01 
+    USE_MINI_MASK = True 
+    MAX_GT_INSTANCES = 500
+    
+
+    
+#5th try: all layers, 100 epochs, coco, score: 0.062
+class BowlConfig(Config): 
+    NAME = "bowl" 
+    GPU_COUNT = 1 
+    IMAGES_PER_GPU = 2 
+    NUM_CLASSES = 1 + 1 
+    IMAGE_MIN_DIM = 256 
+    IMAGE_MAX_DIM = 512 
+    RPN_ANCHOR_SCALES = (4, 8, 16, 32, 64)
+    TRAIN_ROIS_PER_IMAGE = 500 
+    STEPS_PER_EPOCH = 600 // (IMAGES_PER_GPU * GPU_COUNT) 
+    VALIDATION_STEPS = 70 // (IMAGES_PER_GPU * GPU_COUNT) 
+    MEAN_PIXEL = [0, 0, 0] 
+    LEARNING_RATE = 0.01 
+    USE_MINI_MASK = True 
+    MAX_GT_INSTANCES = 500
+    RESNET_ARCHITECTURE = "resnet50"
+"""
+
+
+#4th try: all layers, 100 epochs, imagenet, score: 0.360
+#3rd try: heads only, 25 epochs, imagenet, score: 0.315
+#2nd try: heads only, 100 epochs, imagenet, score:0.336
+"""
+class BowlConfig(Config): 
+    NAME = "bowl" 
+    GPU_COUNT = 1 
+    IMAGES_PER_GPU = 2 
+    NUM_CLASSES = 1 + 1 
+    IMAGE_MIN_DIM = 256 
+    IMAGE_MAX_DIM = 512 
+    RPN_ANCHOR_SCALES = (4, 8, 16, 32, 64)
+    TRAIN_ROIS_PER_IMAGE = 500 
+    STEPS_PER_EPOCH = 600 // (IMAGES_PER_GPU * GPU_COUNT) 
+    VALIDATION_STEPS = 70 // (IMAGES_PER_GPU * GPU_COUNT) 
+    MEAN_PIXEL = [0, 0, 0] 
+    LEARNING_RATE = 0.01 
+    USE_MINI_MASK = True 
+    MAX_GT_INSTANCES = 500
+"""
+    
+
+#1st try, all layers, 100 epochs, imagenet  score: 0.337
+"""
+class BowlConfig(Config):
+    
+    # Give the configuration a recognizable name
+    NAME = "bowl"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1 # background + nuclei
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 600
+
+    STEPS_PER_EPOCH = None
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+
+    USE_MINI_MASK = True
+
+    MAX_GT_INSTANCES = 256
+
+    DETECTION_MAX_INSTANCES = 512
+
+    RESNET_ARCHITECTURE = "resnet50"
+"""
+
+bowl_config = BowlConfig()
+bowl_config.display()
---
+++ train.py
@@ -0,0 +1,111 @@
+import tensorflow as tf
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+
+from mrcnn.config import Config
+from mrcnn import utils
+from mrcnn import model as modellib
+from mrcnn import visualize
+from mrcnn.model import log
+from bowl_dataset import BowlDataset
+
+import numpy as np
+import imgaug
+from bowl_config import bowl_config
+
+cfg = tf.ConfigProto()
+cfg.gpu_options.allow_growth = True
+session = tf.Session(config=cfg)
+
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+def get_ax(rows=1, cols=1, size=8):
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+
+plt.rcParams["figure.figsize"] = [40,40]
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+    
+    
+# Training dataset
+dataset_train = BowlDataset()
+dataset_train.load_bowl('stage1_train_fixed')
+#dataset_train.load_bowl('stage1_train')
+dataset_train.prepare()
+print("Images: {} nClasses: {}".format(len(dataset_train.image_ids), dataset_train.class_names))
+# Validation dataset
+dataset_val= BowlDataset()
+dataset_val.load_bowl('extra_data')
+dataset_val.prepare()
+print("Images: {} nClasses: {}".format(len(dataset_val.image_ids), dataset_val.class_names))
+
+
+### augmentation
+import imgaug
+seq = imgaug.augmenters.Sequential([
+    imgaug.augmenters.Fliplr(0.5), # horizontally flip 50% of all images
+    imgaug.augmenters.Flipud(0.5), # vertically flip 20% of all images
+    imgaug.augmenters.Affine(
+        scale=(1, 2), # scale images to 80-120% of their size, individually per axis
+        rotate=(-90, 90), # rotate by -45 to +45 degrees
+        shear=(-16, 16), # shear by -16 to +16 degrees
+        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        order=[0, 1])  
+])
+
+for image_id in dataset_train.image_ids[:4]:
+    image, image_meta, class_ids, bbox, mask = modellib.load_image_gt(
+        dataset_train, bowl_config, image_id, augment=False, augmentation=seq, use_mini_mask=True)
+    mask = utils.expand_mask(bbox, mask, image.shape)
+
+#### Create data generator
+generator = modellib.data_generator(
+    dataset_train, bowl_config, shuffle=True, batch_size=4, detection_targets=False)
+
+
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=bowl_config, model_dir=MODEL_DIR)
+
+# Which weights to start with?
+init_with = "coco"  # imagenet, coco, or last
+
+if init_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)
+elif init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last()[1], by_name=True)
+    
+
+
+model.train(dataset_train, dataset_val,
+            learning_rate=bowl_config.LEARNING_RATE, 
+            epochs=100, 
+            layers="all")
